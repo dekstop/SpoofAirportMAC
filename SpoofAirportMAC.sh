961a48e884904b1f174fa4b4d9567d914805815f
martind <martin@dekstop.de>
2013-02-07 11:37:38 +0000

    allow manual selection of MAC

--- SpoofAirportMAC.sh
+++ SpoofAirportMAC.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
+mac=00:`RANDFILE=/tmp/.rnd openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//'`
+
 if [ $# == 0 ]; then
 	device=en1
 elif [ $# == 1 ]; then
-	device=$1
+	device="$1"
+elif [ $# == 2 ]; then
+	device="$1"
+	mac="$2"
 else
-	echo "<device>"
+	echo "[device [MAC address]]"
 	exit 1
 fi
 
-echo "Randomising MAC address for ${device}"
-networksetup -setairportpower ${device} on || exit 1
+echo "Setting MAC address of ${device} to ${mac}"
+networksetup -setairportpower "${device}" on || exit 1
 /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z || exit 1
-/sbin/ifconfig ${device} ether 00:`RANDFILE=/tmp/.rnd openssl rand -hex 5 | sed 's/\(..\)/\1:/g; s/.$//'` || exit 1
+/sbin/ifconfig "${device}" ether "${mac}" || exit 1
 networksetup -detectnewhardware || exit 1
